03e6199374ea71cfb8c78aabd537345403754780
Igor Andriyash <igor.andriyash@gmail.com>
2021-11-29 16:25:44 +0100

    C: C minor fix

--- qsa_pwfa/new_grid.py
+++ qsa_pwfa/new_grid.py
@@ -15,18 +15,8 @@ def sum_up_to_r( a, r, r_rings ):
 
 @njit
 def get_psi_part_ng(r, r_rings, dV):
-   
     Psi_r = -0.25 * r**2 + sum_up_to_r( \
             dV * np.log(r / r_rings ), r, r_rings )
-
-    return Psi_r
-
-
-@njit
-def get_psi_ng(r, r_rings, dV, ro):
-   
-    Psi_r = ( dV * ( ( (r > r0).astype(np.int32)  - (r > r_rings).astype(np.int32) ) * np.log(r0 / r) +  \
-                         (r_rings > r).astype(np.int32) * np.log(r_rings / r0)   )).sum()
 
     return Psi_r
 
@@ -38,11 +28,23 @@ def get_Psi_new_grid(r, r_rings, dV, r0):
     Psi += Psi0
     return Psi
 
-@njit    
-def get_Psi_new_grid_new_method(r, r_rings, dV, r0):
+@njit
+def get_psi_ng(r_loc, r, r0, dV):
+    Psi_loc = ( dV * \
+      ( \
+        ( (r_loc > r0).astype(np.int8) - (r_loc > r_rings).astype(np.int8) ) * \
+          np.log( r0 / r_loc ) +  \
+        (r > r_loc).astype(np.int8) * np.log( r / r0 ) ) \
+      ).sum()
 
-    Psi = get_psi_ng(r, r_rings, dV, r0)
+    return Psi_loc
+
+
+@njit(parallel=True)
+def get_Psi_new_grid_new_method(r_grid, r_rings, r0_rings, dV):
+
+    for ir in prange(r_grid.size):
+        Psi[ir] = get_psi_ng(r_grid[ir], r_rings, r0_rings, dV)
 
     return Psi
 
-    
