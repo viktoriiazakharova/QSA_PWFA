cc22ffb32d48b20dd80de74f900b8def82c29be7
viktoriiazakharova <Zakharova98.ru@yandex.ru>
2021-11-29 16:02:04 +0100

    added new method for the new grid

--- qsa_pwfa/new_grid.py
+++ qsa_pwfa/new_grid.py
@@ -21,11 +21,28 @@ def get_psi_part_ng(r, r_rings, dV):
 
     return Psi_r
 
+
+@njit
+def get_psi_ng(r, r_rings, dV, ro):
+   
+    Psi_r = ( dV * ( ( (r > r0).astype(np.int32)  - (r > r_rings).astype(np.int32) ) * np.log(r0 / r) +  \
+                         (r_rings > r).astype(np.int32) * np.log(r_rings / r0)   )).sum()
+
+    return Psi_r
+
+
 @njit
 def get_Psi_new_grid(r, r_rings, dV, r0):
     Psi0 = (dV * np.log(r_rings / r0 )).sum()
     Psi = get_psi_part_ng(r, r_rings, dV)
     Psi += Psi0
-    
     return Psi
+
+@njit    
+def get_Psi_new_grid_new_method(r, r_rings, dV, r0):
+
+    Psi = get_psi_ng(r, r_rings, dV, r0)
+
+    return Psi
+
     
