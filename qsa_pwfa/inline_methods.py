5568ec4fecb6a1f4fae6c40cb818b85d33d75178
hightower8083 <igor.andriyash@gmail.com>
2022-05-28 22:27:15 +0200

    - optimized inline methods

--- qsa_pwfa/inline_methods.py
+++ qsa_pwfa/inline_methods.py
@@ -53,6 +53,8 @@ def get_Density_inline(Density_target, r_target, dr_target,
 
 @njit(parallel=True)
 def get_Psi_inline( Psi_target, r_target, r_source, r0_source, dQ_source ):
+    r0_source_inv = 1. / r0_source
+    log_r_r0_source = np.log(r_source * r0_source_inv)
 
     for ir in prange(r_target.size):
         H_r_m_rj =  fast_compare(r_source, r_target[ir])
@@ -60,8 +62,8 @@ def get_Psi_inline( Psi_target, r_target, r_source, r0_source, dQ_source ):
         H_r_m_r0j = fast_compare(r0_source, r_target[ir])
 
         Psi_target[ir] += np.sum( dQ_source * \
-            ( ( H_r_m_r0j - H_r_m_rj ) * np.log(r_target[ir] / r0_source) \
-              - H_rj_m_r_excl * np.log(r_source / r0_source) ) )
+            ( ( H_r_m_r0j - H_r_m_rj ) * np.log(r_target[ir] * r0_source_inv) \
+              - H_rj_m_r_excl * log_r_r0_source ) )
 
     return Psi_target
 
@@ -92,50 +94,60 @@ def get_dPsi_dr_inline(dPsi_dr_target, r_target, n_p_source, r_source,
 @njit(parallel=True)
 def get_dAz_dr_inline(dAz_dr_target, r_target, r_source,
                       v_z_source, dQ_source):
+    
+    dJz_source = dQ_source * v_z_source / (1.-v_z_source)
 
     for ir in prange(r_target.size):
         H_r_m_rj =  fast_compare(r_source, r_target[ir])
-        dAz_dr_target[ir] += - np.sum(dQ_source * v_z_source / (1.-v_z_source) \
-                                * H_r_m_rj ) / r_target[ir]
+        dAz_dr_target[ir] += - np.sum(dJz_source * H_r_m_rj ) / r_target[ir]
 
     return dAz_dr_target
 
 @njit(parallel=True)
 def get_dAz_dr_bunch_inline(dAz_dr_target, r_target, r_source,
                       v_z_source, dQ_source):
+    
+    v_z_dQ_source = dQ_source * v_z_source
 
     for ir in prange(r_target.size):
         H_r_m_rj =  fast_compare(r_source, r_target[ir])
-        dAz_dr_target[ir] += -np.sum(dQ_source * v_z_source \
-                                * H_r_m_rj ) / r_target[ir]
+        dAz_dr_target[ir] += -np.sum(v_z_dQ_source * H_r_m_rj ) / r_target[ir]
 
     return dAz_dr_target
 
 @njit(parallel=True)
 def get_dPsi_dxi_inline(dPsi_dxi_target, r_target, r_source,
                         r0_source, dr_dxi_source, dQ_source):
+    
+    dQ_dr_dxi_r_source = dQ_source * dr_dxi_source/r_source
 
     for ir in prange(r_target.size):
         H_r_m_rj =  fast_compare(r_source, r_target[ir])
         H_rj_m_r_excl = 1 - H_r_m_rj
-        dPsi_dxi_target[ir] += -np.sum( dQ_source * dr_dxi_source \
-                                      * H_rj_m_r_excl / r_source)
+        dPsi_dxi_target[ir] += -np.sum( dQ_dr_dxi_r_source \
+                                      * H_rj_m_r_excl)
 
     return dPsi_dxi_target
 
 @njit(parallel=True)
 def get_dAr_dxi_inline(dAr_dxi_target, r_target, r_source, dr_dxi_source,
                        d2r_dxi2_source, dQ_source):
-
+    
+    
+    r_source_inv = 1./r_source
+    dr_dxi_source2 = dr_dxi_source**2
+    r_source2 = r_source**2
+    
+    Term2 = 0.5 * ( d2r_dxi2_source * r_source_inv \
+                - (dr_dxi_source * r_source_inv) ** 2 )
+    
     for ir in prange(r_target.size):
         H_r_m_rj =  fast_compare(r_source, r_target[ir])
         H_rj_m_r_excl = 1 - H_r_m_rj
-        r_source_inv = 1./r_source
+        
         dAr_dxi_target[ir] += 1. / r_target[ir] * np.sum ( dQ_source \
-            * ( dr_dxi_source**2 * H_r_m_rj \
-              + 0.5 * ( d2r_dxi2_source * r_source_inv \
-                       - (dr_dxi_source * r_source_inv) ** 2 ) \
-              * (r_target[ir]**2 * H_rj_m_r_excl  + r_source**2 * H_r_m_rj)
+            * ( dr_dxi_source2 * H_r_m_rj \
+              + Term2 * (r_target[ir]**2 * H_rj_m_r_excl  + r_source2 * H_r_m_rj)
             ) )
 
     return dAr_dxi_target
