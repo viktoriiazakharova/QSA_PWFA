- bug-fix in xi-refined d_xi - removed use of np.gradient in r-grid generation - updated examples

qsa_pwfa/simulation.py
@@ -42,7 +42,9 @@ class Simulation:
             self.L_xi = self.xi.max()
             self.N_xi = self.xi.size
 
-        self.dxi = np.gradient(self.xi)
+        self.dxi = np.zeros_like(self.xi)
+        self.dxi[1:] = self.xi[1:] - self.xi[:-1]
+        self.dxi[0] = self.dxi[1]
 
     def _advance_xi(self, iter_max, rel_err_max, mixing_factor):
 

qsa_pwfa/species.py
@@ -31,18 +31,21 @@ class BaseSpecie:
 
         if self.user_grid:
             self.r0 = r_grid_user.copy()
-            self.dr0 = np.gradient(self.r0)
+            if (self.r0[0] == 0.0):
+                self.r0 +=  0.5 * self.r0[1]
+                print('warning: origin of r_grid_user is adjusted')            
+            
+            self.dr0 = np.zeros_like(self.r0)
+            self.dr0[1:] = self.r0[1:] - self.r0[:-1]
+            self.dr0[0] = self.r0[0]
             self.N_r = self.r0.size
-            if (self.r0[0] != 0.5 * self.dr0[0]):
-                self.r0 = self.r0 - self.r0[0] + 0.5 * self.dr0[0]
-                print('warning: origin of r_grid_user is adjusted')
             self.L_r = self.r0.max()
         else:
             self.L_r = L_r
             self.N_r = N_r
             self.r0 = L_r / N_r * np.arange(N_r)
-            self.dr0 = np.gradient(self.r0)
-            self.r0 += 0.5*self.dr0
+            self.dr0 = L_r / N_r * np.ones_like(self.r0)
+            self.r0 += 0.5 * self.dr0
 
         self.dV = self.dr0 * (self.r0 - 0.5*self.dr0)
         self.dV[0] = 0.125 * self.dr0[0]**2      
@@ -155,7 +158,7 @@ class BunchSpecie(BaseSpecie):
         L_r = self.truncate_factor * self.sigma_r
         
         r = L_r / self.N_r * np.arange(self.N_r)
-        dr0 = np.gradient(r)
+        dr0 = L_r / self.N_r * np.ones_like(r)
         r += 0.5 * dr0
 
         self.r_bunch = r[None,:] * np.ones_like(xi)[:, None]
@@ -167,7 +170,7 @@ class BunchSpecie(BaseSpecie):
         self.dV = self.dQ_bunch[0, :].copy()
 
         self.dQ_bunch *= self.q * self.n_p * self.dens_func(\
-            self.r_bunch, self.xi_bunch)
+            self.r_bunch - 0.5 * dr0, self.xi_bunch)
         
         self.p_r_bunch = self.eps_r / self.sigma_r * np.random.randn(*self.r_bunch.shape)
         gamma_p = self.gamma_b + self.delta_gamma * np.random.randn(*self.r_bunch.shape) 